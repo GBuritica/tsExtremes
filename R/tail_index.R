d4fda7b23d15a420c9aec1acf54a42837aa35dd1
Gloria Buriticá <gloriaburit@gmail.com>
2024-04-15 19:30:28 +0200

    Update tail_index.R

--- R/tail_index.R
+++ R/tail_index.R
@@ -2,8 +2,8 @@
 #' @description This function computes Hill (tail)-index estimate of a stationary time series.
 #'
 #' @param path (vector of univariate observations)
-#' @param k0   (vector of order statistics to compute Hill estimates).
-#'             It uses k0 = n*0.05 otherwise
+#' @param k1   (vector of order statistics to compute Hill estimates).
+#'             It uses k1 = n*0.05 otherwise
 #' @param plot (T or F if a plot should be shown).
 #'
 #' @return A data.frame of hill estimates as a function of k.
@@ -13,23 +13,23 @@
 #' hillestimator(rainfall$LANVEOC[rainfall$SEASON=='SPRING'], 1:200)
 #' hillestimator(rainfall$LANVEOC[rainfall$SEASON=='SPRING'], plot=T)
 
-hillestimator <- function(path, k0=NULL, plot=F){
+hillestimator <- function(path, k1=NULL, plot=F){
   n      <- length(path)
-  if(is.null(k0)){
-    if(plot) k0 <- 1:floor(n*0.05)
-    else     k0 <- floor(n*0.05)
+  if(is.null(k1)){
+    if(plot) k1 <- 1:floor(n*0.05)
+    else     k1 <- floor(n*0.05)
   }                                   ## Initialize k
-  sorted <- rev( sort(path, partial = n-rev(k0)+1 ) )    ## Computes order statistics
-  hill   <- mean( log(sorted[1:max(1,(k0[1]-1))]/sorted[k0[1]]) )
+  sorted <- rev( sort(path, partial = n-rev(k1)+1 ) )    ## Computes order statistics
+  hill   <- mean( log(sorted[1:max(1,(k1[1]-1))]/sorted[k1[1]]) )
 
-  if(length(k0) > 1){
-    for(k in 2:length(k0)){
-      sums  <- ( hill[(k-1)] + log( sorted[ k0[(k-1)] ] ) )*(k0[(k-1)]-1)
-      hill  <- c(hill,  ( (sums + sum(log( sorted[ (k0[(k-1)]):(k0[k]-1) ] )) )/(k0[k]-1)  - log(sorted[k0[k]] ))  )     ## Computes Hill
+  if(length(k1) > 1){
+    for(k in 2:length(k1)){
+      sums  <- ( hill[(k-1)] + log( sorted[ k1[(k-1)] ] ) )*(k1[(k-1)]-1)
+      hill  <- c(hill,  ( (sums + sum(log( sorted[ (k1[(k-1)]):(k1[k]-1) ] )) )/(k1[k]-1)  - log(sorted[k1[k]] ))  )     ## Computes Hill
     }
   }
 
-  hill <- data.frame("k" = k0, "Hill.estimate" = hill)
+  hill <- data.frame("k" = k1, "Hill.estimate" = hill)
 
   if(plot){
     plot(hill, type = "l", ylab = "Tail-index", main = "Hill Plot" )
